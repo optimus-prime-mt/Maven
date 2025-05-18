11533ab354a5960a59239a23c9537fe9b9af9931
Pratush M T <112371780+optimus-prime-mt@users.noreply.github.com>
2025-05-18 09:34:35 +0530

    Update LoginTest.java

--- LoginTest.java
+++ LoginTest.java
@@ -1,19 +1,26 @@
+package com.example;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class LoginTest {
+public class Main {
     public static void main(String[] args) throws InterruptedException {
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.saucedemo.com");
         driver.manage().window().maximize();
         Thread.sleep(2000);
-        driver.findElement(By.id("user- name")).sendKeys("standard_user");
+
+        // Corrected the ID for username field
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
         Thread.sleep(2000);
+
         driver.findElement(By.id("password")).sendKeys("secret_sauce");
         Thread.sleep(2000);
+
         driver.findElement(By.id("login-button")).click();
         Thread.sleep(2000);
+
         driver.quit();
     }
 }
